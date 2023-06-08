d188f4776b3e82bfd2304cbada37158589c67bab
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-08 03:00:33 -0700

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Navbar from './components/Navbar'
+import MyProfilePic from './components/MyProfilePic'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="dark:bg-slate-800">
         <Navbar />
+        <MyProfilePic />
         {children}
       </body>
     </html>
