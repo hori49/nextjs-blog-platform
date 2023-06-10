2a40d71c023fa92b498ba1020f0a4564baf4aece
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-10 01:40:58 -0700

    changed article format

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -42,7 +42,6 @@ export default async function Post({ params }: { params: { postId: string } }) {
 
     return (
         <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
             <p className="mt-0">
                 {pubDate}
             </p>
