b8a07d4d07281b319f9cb686b264d6aa689d8350
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-10 20:23:54 -0700

    code refactoring

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -10,25 +10,25 @@ export function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory);
   const allPostsData = fileNames.map((fileName) => {
-      // Remove ".md" from file name to get id
-      const id = fileName.replace(/\.md$/, '');
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, '');
 
-      // Read markdown file as string
-      const fullPath = path.join(postsDirectory, fileName);
-      const fileContents = fs.readFileSync(fullPath, 'utf8');
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-      // Use gray-matter to parse the post metadata section
-      const matterResult = matter(fileContents);
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
 
-      const blogPost: BlogPost = {
-          id,
-          title: matterResult.data.title,
-          date: matterResult.data.date,
-          thumbnail: matterResult.data.thumbnail,
-      }
+    const blogPost: BlogPost = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      thumbnail: matterResult.data.thumbnail,
+    }
 
-      // Combine the data with the id
-      return blogPost
+    // Combine the data with the id
+    return blogPost
   });
   // Sort posts by date
   return allPostsData.sort((a, b) => a.date < b.date ? 1 : -1);
@@ -42,17 +42,17 @@ export async function getPostData(id: string) {
   const matterResult = matter(fileContents);
 
   const processedContent = await remark()
-      .use(html)
-      .process(matterResult.content);
+    .use(html)
+    .process(matterResult.content);
 
   const contentHtml = processedContent.toString();
 
   const blogPostWithHTML: BlogPost & { contentHtml: string } = {
-      id,
-      title: matterResult.data.title,
-      date: matterResult.data.date,
-      thumbnail: matterResult.data.thumbnail,
-      contentHtml,
+    id,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    thumbnail: matterResult.data.thumbnail,
+    contentHtml,
   }
 
   // Combine the data with the id
