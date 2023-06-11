8d5e0e991d521e80d145a386dbe23ce48da496d7
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-10 19:40:17 -0700

    code refactoring

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -5,51 +5,48 @@ import Image from 'next/image'
 import Link from "next/link"
 
 export function generateStaticParams() {
-    const posts = getSortedPostsData()
+  const posts = getSortedPostsData()
 
-    return posts.map((post) => ({
-        postId: post.id
-    }))
+  return posts.map((post) => ({
+    postId: post.id
+  }))
 }
 
 export function generateMetadata({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData()
+  const { postId } = params
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+  const post = posts.find(post => post.id === postId)
 
-    const post = posts.find(post => post.id === postId)
-
-    if (!post) {
-        return {
-            title: 'Post Not Found'
-        }
-    }
-
+  if (!post) {
     return {
-        title: post.title,
-    }
+      title: 'Post Not Found'
+  }
+  }
+
+  return {
+    title: post.title,
+  }
 }
 
 export default async function Post({ params }: { params: { postId: string } }) {
+  const posts = getSortedPostsData()
+  const { postId } = params
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+  if (!posts.find(post => post.id === postId)) notFound()
 
-    if (!posts.find(post => post.id === postId)) notFound()
+  const { title, date, thumbnail, contentHtml } = await getPostData(postId)
+  const pubDate = getFormattedDate(date)
 
-    const { title, date, thumbnail, contentHtml } = await getPostData(postId)
-
-    const pubDate = getFormattedDate(date)
-
-    return (
-      <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+  return (
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
       <Image src={thumbnail} alt={title} width={500} height={300}/>
       <h1 className="text-3xl mt-4 mb-0">{title}</h1>
       <p className="mt-0">{pubDate}</p>
       <article>
-          <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-          <p><Link href="/">← Back to home</Link></p>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p><Link href="/">← Back to home</Link></p>
       </article>
-  </main>
-    )
+    </main>
+  )
 }
