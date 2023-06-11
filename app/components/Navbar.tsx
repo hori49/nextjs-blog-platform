c0e2f30e2fed5bded2168ef055eb512430bc1e5e
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-10 21:39:47 -0700

    Changed Navbar design

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
         <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-          <Link href="/" className="text-white/90 no-underline hover:text-white flex items-center">
+          <Link href="/" className="text-white/90 no-underline hover:text-white flex items-center gap-2">
             <FaHome /><span>HOME</span>
           </Link>
         </h1>
