4cc4732a78a3ce60780cc6e484ec805246f63585
Daisuke Horiuchi <dhoriuchi001@gmail.com>
2023-06-10 21:11:41 -0700

    Changed articles list design

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -11,14 +11,17 @@ export default function ListItem({ post }: Props) {
 
   return (
     <li className="mt-4 text-2xl dark:text-white/90">
-    <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/posts/${id}`}>
-      <div>
-        <img src={thumbnail} alt={`${title} thumbnail`} />
-        {title}
-      </div>
-    </Link>
-    <br />
-    <p className="text-sm mt-1">{formattedDate}</p>
-</li>
+      <Link className="underline hover:text-black/70 dark:hover:text-white" href={`/posts/${id}`}>
+        <div className="flex items-center gap-5">
+          <div style={{ flex: 1 }}>
+            <img src={thumbnail} alt={`${title} thumbnail`} className="mr-4" />
+          </div>
+          <div style={{ flex: 2 }}>
+            <h2>{title}</h2>
+            <p className="text-sm mt-1">{formattedDate}</p>
+          </div>
+        </div>
+      </Link>
+    </li>
   )
 }
